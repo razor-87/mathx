671dc1cb6eadfcbebd725e10fcdc9cffb73a88ff
Sergei Razorenov <sergrazor87@gmail.com>
2024-04-11 12:08:08 +0300

    add optimum unrolling for sum
                     │   std.txt    │              unroll.txt               │
                     │     B/s      │      B/s       vs base                │
    VecSum/~16KB-16    4.971Gi ± 8%   27.557Gi ± 2%  +454.38% (p=0.000 n=8)
    VecSum/~64KB-16    4.860Gi ± 2%   27.160Gi ± 3%  +458.81% (p=0.000 n=8)
    VecSum/~256KB-16   4.859Gi ± 5%   27.584Gi ± 5%  +467.74% (p=0.000 n=8)
    VecSum/~1MB-16     4.763Gi ± 3%   26.852Gi ± 4%  +463.73% (p=0.000 n=8)
    VecSum/~4MB-16     4.829Gi ± 4%   26.910Gi ± 6%  +457.26% (p=0.000 n=8)
    VecSum/~16MB-16    4.730Gi ± 2%   16.512Gi ± 2%  +249.12% (p=0.000 n=8)
    VecSum/~64MB-16    4.701Gi ± 3%   14.374Gi ± 1%  +205.76% (p=0.000 n=8)
    VecSum/~128MB-16   4.750Gi ± 3%   14.477Gi ± 2%  +204.80% (p=0.000 n=8)
    VecSum/~256MB-16   4.764Gi ± 3%   14.848Gi ± 2%  +211.65% (p=0.000 n=8)
    geomean            4.802Gi         20.90Gi       +335.22%

--- vec/sum.go
+++ vec/sum.go
@@ -1,8 +1,18 @@
 package vec
 
 func Sum(xs []float64) (ret float64) {
-	for i := range xs {
-		ret += xs[i]
+	const k = 8
+	if r := len(xs) % k; r != 0 {
+		for i := range xs[:r] {
+			ret += xs[i]
+		}
+		xs = xs[r:]
 	}
+	var s []float64
+	for i := 0; i < len(xs); i += k {
+		s = xs[i : i+k : i+k]
+		ret += s[0] + s[1] + s[2] + s[3] + s[4] + s[5] + s[6] + s[7]
+	}
+
 	return ret
 }
