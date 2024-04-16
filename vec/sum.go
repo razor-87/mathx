e0cb88b2408000c5538b0cc93c76b9fa7b847286
Sergei Razorenov <sergrazor87@gmail.com>
2024-04-16 15:25:02 +0300

    improve sum func

--- vec/sum.go
+++ vec/sum.go
@@ -2,18 +2,13 @@ package vec
 
 import "github.com/razor-87/mathx"
 
-func Sum[T mathx.Vector[E], E mathx.Floaty](xs T) (ret E) {
+func Sum[T mathx.Vector[E], E mathx.Floaty](v T) (ret E) {
 	const k = 8
-	if r := len(xs) % k; r != 0 {
-		for i := range xs[:r] {
-			ret += xs[i]
-		}
-		xs = xs[r:]
+	for ; len(v) >= k; v = v[k:] {
+		ret += v[0] + v[1] + v[2] + v[3] + v[4] + v[5] + v[6] + v[7]
 	}
-	var s T
-	for i := 0; i < len(xs); i += k {
-		s = xs[i : i+k : i+k]
-		ret += s[0] + s[1] + s[2] + s[3] + s[4] + s[5] + s[6] + s[7]
+	for i := range v {
+		ret += v[i]
 	}
 
 	return ret
