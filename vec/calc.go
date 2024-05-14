424021c7bb2bef68a95d473e01866efaa75e1bb2
Sergei Razorenov <sergrazor87@gmail.com>
2024-05-14 19:51:35 +0300

    add single size for loop unrolling

--- vec/calc.go
+++ vec/calc.go
@@ -2,6 +2,8 @@ package vec
 
 import "github.com/razor-87/mathx"
 
+const stride = 8
+
 func Add[T mathx.Vector[E], E mathx.Floaty](v, w T) {
 	for i := 0; i < len(v) && i < len(w); i++ {
 		v[i] += w[i]
@@ -15,8 +17,7 @@ func Scale[T mathx.Vector[E], E mathx.Floaty](v T, c E) {
 }
 
 func Sum[T mathx.Vector[E], E mathx.Floaty](v T) (ret E) {
-	const k = 8
-	for ; len(v) >= k; v = v[k:] {
+	for ; len(v) >= stride; v = v[stride:] {
 		ret += v[0] + v[1] + v[2] + v[3] + v[4] + v[5] + v[6] + v[7]
 	}
 	for i := range v {
